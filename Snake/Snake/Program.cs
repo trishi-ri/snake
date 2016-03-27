f7ed59ac38630ef31544fc4cf60ef9a5b89b442d
trishi-ri <trishi-ri@ya.ru>
2016-03-27 10:29:58 +0300

    add list of points

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -17,6 +17,10 @@ namespace Snake
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
 
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
+
             Console.ReadLine();
         }
     }
