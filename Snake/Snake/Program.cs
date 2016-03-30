613d1d1661593dacb60cc5773525f7263bbd13e7
trishi-ri <trishi-ri@ya.ru>
2016-03-30 12:54:11 +0300

    add reaction on keys

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -9,7 +9,6 @@ namespace Snake
         {
 
             Console.SetBufferSize(80, 25);
-
             //frames
             HorizontalLine upF = new HorizontalLine(0, 78, 0, '+');
             upF.Draw();
@@ -20,21 +19,23 @@ namespace Snake
             VerticalLine rightF = new VerticalLine(0, 24, 78, '+');
             rightF.Draw();
 
+            //points
             Point p = new Point(4, 5, '*');
             Snake snake = new Snake(p, 4, Direction.RIGHT);
             snake.Draw();
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
+
+            while (true)
+            {
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    if (key.Key == ConsoleKey.S)
+                        break;
+                    snake.HandleKey(key.Key);
+                }
+                Thread.Sleep(300);
+                snake.Move();
+            }
 
             Console.ReadLine();
         }
