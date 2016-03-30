7587694afe1681843a49069928ddb73975a831f3
trishi-ri <trishi-ri@ya.ru>
2016-03-30 11:26:57 +0300

    new classes Snake and direction

--- Snake/Snake/Point.cs
+++ Snake/Snake/Point.cs
@@ -17,13 +17,44 @@ namespace Snake
             x = _x;
             y = _y;
             sym = _sym;
+        }
 
+        public Point(Point p)
+        {
+            x = p.x;
+            y = p.y;
+            sym = p.sym;
+        }
+
+        public void Move(int offset, Direction direction)
+        {
+            if (direction == Direction.RIGHT)
+            {
+                x = x + offset;
+            }
+            else if (direction == Direction.LEFT)
+            {
+                x = x - offset;
+            }
+            else if (direction == Direction.UP)
+            {
+                y = y + offset;
+            }
+            else
+            {
+                y = y - offset;
+            }
         }
 
         public void Draw()
         {
             Console.SetCursorPosition(x, y);
             Console.Write(sym);
+        }
+
+        public override string ToString()
+        {
+            return x + ", " + y + ", " + sym;
         }
     }
 }
---
+++ Snake/Snake/Snake.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Snake
+{
+    class Snake : Figure
+    {
+        public Snake(Point tail, int length, Direction direction)
+        {
+            pList = new List<Point>();
+            for (int i = 0; i < length; i++)
+            {
+                Point p = new Point(tail);
+                p.Move(i, direction);
+                pList.Add(p);
+            }
+        }
+    }
+}
