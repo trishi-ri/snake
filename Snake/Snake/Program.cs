4e9c955485c373134bda1f28952d0d1ba07e9f63
trishi-ri <trishi-ri@ya.ru>
2016-04-01 18:21:39 +0300

    add endgame

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Threading;
 
 namespace Snake
@@ -11,12 +10,31 @@ namespace Snake
 
             Console.SetBufferSize(80, 25);
 
+            int start_length = 4;
+            int max_length = start_length;
+            int count_games = 0;
+            int snake_length;
+            do
+            {
+                count_games++;
+                snake_length = NewGame(start_length);
+                if (snake_length > max_length)
+                    max_length = snake_length;
+
+            }
+            while (WriteGameOver(snake_length, max_length, count_games) == "");
+
+        }
+
+        static int NewGame(int start_length)
+        {
+            Console.Clear();
             Walls walls = new Walls(80, 25);
             walls.Draw();
 
             //points
             Point p = new Point(4, 5, '*');
-            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            Snake snake = new Snake(p, start_length, Direction.RIGHT);
             Draw(snake);
 
             FoodCreator foodCreator = new FoodCreator(80, 25, '@');
@@ -45,6 +63,32 @@ namespace Snake
                 }
 
             }
+            return snake.Length();
+        }
+
+        static string WriteGameOver(int snake_length, int max_length, int count_games)
+        {
+            int xOffset = 20;
+            int yOffset = 6;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.SetCursorPosition(xOffset, yOffset++);
+            WriteText("==========================================", xOffset, yOffset++);
+            WriteText("К О Н Е Ц    И Г Р Ы", xOffset + 10, yOffset++);
+            yOffset++;
+            WriteText("номер игры в сесии: " + count_games, xOffset + 2, yOffset++);
+            WriteText("длина змейки в последней игре: " + snake_length, xOffset + 2, yOffset++);
+            WriteText("максимальная длина змейки в сессии: " + max_length, xOffset + 2, yOffset++);
+            yOffset++;
+            WriteText("начать новую игру - Enter", xOffset + 2, yOffset++);
+            WriteText("==========================================", xOffset, yOffset++);
+            return Console.ReadLine();
+
+        }
+
+        static void WriteText(string text, int xOffset, int yOffset)
+        {
+            Console.SetCursorPosition(xOffset, yOffset);
+            Console.WriteLine(text);
         }
 
         static void Draw(Figure figure)
